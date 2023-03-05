cf7908cbe41b7c1d78a5ddca7580b6fffbc16ebb
Brad Coleman <bradcoleman60@gmail.com>
2023-03-04 21:20:53 -0800

    adds basic form for contact page, not format yet

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import '../style.css'
+import { validateEmail } from '../utils/helpers';
 
-export default function Contact() {
+ function Form() {
+
+  const [name, setName] = useState(''); 
+  const [email, setEmail] = useState(''); 
+  const [message, setMessage] = useState(''); 
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    //Get the name from the form
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type we set the state of name, email or message
+    if (inputType === 'name'){
+      setName(inputValue)
+    } else if (inputType === 'email'){
+      setEmail(inputValue)
+    } else {
+      setMessage(inputValue);
+    }
+
+  };
+  
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)){
+      setErrorMessage('Email is invalid');
+      return;
+    }
+    alert(`Hello ${name}`)
+
+    //Set these fields back to empty if all validations work
+    setName('');
+    setEmail('');
+    setMessage('');
+
+  };
+
+  
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <form>
+        <input
+        value={name}
+        name="name"
+        onChange={handleInputChange}
+        type="text"
+        placeholder="name"
+        />
+        <input 
+        value={email}
+        name="email"
+        onChange={handleInputChange}
+        type="email"
+        placeholder="email"
+        />
+        
+        <input
+        value={message}
+        name="message"
+        onChange={handleInputChange}
+        type="text"
+        placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className='error-text'>{errorMessage}</p>
+          </div>
+      )}
+
     </div>
   );
 }
+
+export default Form;
