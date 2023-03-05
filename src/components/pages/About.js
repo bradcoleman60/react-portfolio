4b155784039d15699081f2a2f4a847db5ab26532
Brad Coleman <bradcoleman60@gmail.com>
2023-03-05 12:53:45 -0800

    adds about me page with a image and formats resume page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import '../styles/About.css';
 
 export default function About() {
   return (
-    <div>
+    <div className="container">
+      <div className='row'>
+        <div className='col-4'>
+          <img src='../images/bc.JPG' alt='Brad Coleman' width="120%"></img>
+          </div>
+      <div className='col-8'>
       <h1>About Me</h1>
-      <p>
+      <h5>
       My name is Brad Coleman. I am fairly new to web development but have considered it a hobby for several years and have hacked my way through learning various aspects including php, html and mysql.  I am currently enrolled in the Cal Berkeley Extension Web Development Boot camp and am excited to learn web development more holistically.  I have spent my earlier career working as a corporate controller / CPA.
-      </p>
+      </h5>
+    </div>
+    </div>
     </div>
   );
 }
