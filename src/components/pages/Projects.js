93a6044afbc29d8090399f5aa6e691deedcb6c85
Brad Coleman <bradcoleman60@gmail.com>
2023-03-05 23:18:30 -0800

    fixes some formatting on project page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -7,108 +7,74 @@ export default function Projects() {
   const projects = [
     {
       url: "https://bradcoleman60.github.io/GitYourGameOn/",
+      github: "https://github.com/bradcoleman60/GitYourGameOn",
       backgroundImage: "url('/images/nba_project.jpg')",
     },
     {
       url: "https://bradcoleman60.github.io/marketing-refactor/",
+      github: "https://github.com/bradcoleman60/marketing-refactor",
       backgroundImage: "url('/images/digital-marketing-meeting.jpg')",
+    },
+    {
+      url: "https://afternoon-badlands-25422.herokuapp.com/",
+      github: "https://github.com/bradcoleman60/tech-blog",
+      backgroundImage: "url('/images/tech_blog.jpg')",
+    },
+    {
+      url: "https://bradcoleman60.github.io/weather-dashboard/",
+      github: "https://github.com/bradcoleman60/weather-dashboard",
+      backgroundImage: "url('/images/weather.jpg')",
+    },
+    {
+      url: "https://pure-depths-43598.herokuapp.com/",
+      github: "https://github.com/bradcoleman60/project-panda",
+      backgroundImage: "url('/images/panda.jpg')",
+    },
+    
+    {
+      url: "https://bradcoleman60.github.io/javascript-quiz/",
+      github: "https://github.com/bradcoleman60/javascript-quiz",
+      backgroundImage: "url('/images/javascript_quiz.jpg')",
     },
   ];
 
   function handleClick(url) {
-    window.location.href = url;
+    window.open(url, "_blank");
   }
 
   return (
     <div className="container border border-primary">
-      {projects.map((item, index) => (
-        <div
-          key={index}
-          className="col-md-4 border border-primary"
-          style={{
-            backgroundImage: item.backgroundImage,
-            height: "300px",
-            width: "300px",
-            backgroundSize: "cover",
-          }}
-          onClick={() => handleClick(item.url)}
-        >
-          test
-        </div>
-      ))}
-
-      {/* <div class="col-lg-12 border border-primary">
-        <div class="row border border-primary">
-          <a
-            href="https://bradcoleman60.github.io/GitYourGameOn/"
-            target="_blank"
-            rel="noreferrer"
+      <div className="row">
+        {projects.map((item, index) => (
+          <div
+            key={index}
+            className="col-md-4 col-lg-4 col-xl-4 px-5 border border-primary position-relative"
+            style={{
+              backgroundImage: item.backgroundImage,
+              height: "400px",
+              width: "400px",
+              backgroundSize: "cover",
+              padding: "30px"
+            }}
+            onClick={() => handleClick(item.url)}
           >
-            <div class="col-md-4 border border-primary" id="card_1">
-              col-sm
+            <div
+              className="bg-light p-2 position-absolute"
+              style={{
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              <a href={item.github} target="_blank" rel="noopener noreferrer">
+                GitHub Repo
+              </a>
             </div>
-          </a>
-          <div class="col-md-4 border border-primary" id="card_2">
-            col-sm
-          </div>
-          <div class="col-md-4 border border-primary">col-sm</div>
         </div>
-        <div class="row border border-primary">
-          <div class="col-md-4 border border-primary">col-sm</div>
-          <div class="col-md-4 border border-primary">col-sm</div>
-          <div class="col-md-4 border border-primary">col-sm</div>
-        </div>
-      </div> */}
+        ))}
+      </div>
     </div>
-
-    //     <section class="main-container">
-
-    // {/* Container_1 includes the marquee project and is the most prominent on the  */}
-    // {/* page.  */}
-
-    //     <section class="container_1" id="main-project">
-    //         <a href="https://bradcoleman60.github.io/GitYourGameOn/" target="_blank">
-    //         <div class="card_1">
-    //             <h1>GitYourGameOn</h1>
-    //             <p>My first team project building a site from scratch </p>
-    //         </div>
-    //         </a>
-    //     </section>
-
-    // {/* <!-- Container_2 includes the other noteable projects --> */}
-
-    //     <section class="container_2">
-
-    //         <a href="https://bradcoleman60.github.io/prework-study-guide/" target="_blank">
-    //             <div class="card_3">
-    //                 <h1>Pre Work Site</h1>
-    //                 <p>My first static webpage using HTML and CSS and a little Java Script.</p>
-    //             </div>
-    //         </a>
-
-    //         <a href="https://bradcoleman60.github.io/landing-page-two/" target="_blank">
-    //             <div class="card_2">
-    //                 <h1>First Landing Page </h1>
-    //                 <p>Creation of my First Landing Page.</p>
-    //             </div>
-    //         </a>
-
-    //         <a href="https://bradcoleman60.github.io/marketing-refactor/" target="_blank">
-    //             <div class="card_2">
-    //                 <h1>Code Refactor</h1>
-    //                 <p>In this project I refactored existing HMTL and CSS to add semantic elements and instituted aaccessibility standards.</p>
-    //             </div>
-    //         </a>
-
-    //         <a href="#" target="_blank">
-    //             <div class="card_2">
-    //                 <h1>React</h1>
-    //                 <p>Coming Soon.</p>
-    //             </div>
-    //         </a>
-
-    //     </section>
-
-    // </section>
   );
 }
